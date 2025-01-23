a66a4bc8a3fa77eeb95103c151f2ba642858d090
dilarauluturhan <dilarauluturhan@outlook.com>
2025-01-23 14:39:39 +0300

    feat:profile-screen

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,11 +1,106 @@
-import { View, Text } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  ImageSourcePropType,
+} from "react-native";
 import React from "react";
 
-const Profile = () => {
-  return (
-    <View>
-      <Text>Profile</Text>
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useDispatch } from "react-redux";
+import { logout } from "@/redux/userSlice";
+
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { settings } from "@/constants/data";
+
+interface SettingsItemProp {
+  icon: ImageSourcePropType;
+  title: string;
+  onPress?: () => void;
+  textStyle?: string;
+  showArrow?: boolean;
+}
+
+const SettingsItem = ({
+  icon,
+  title,
+  onPress,
+  textStyle,
+  showArrow = true,
+}: SettingsItemProp) => (
+  <TouchableOpacity
+    onPress={onPress}
+    className="flex flex-row items-center justify-between py-3"
+  >
+    <View className="flex flex-row items-center gap-3">
+      <Image source={icon} />
+      <Text className={`text-lg font-medium text-black-300 ${textStyle}`}>
+        {title}
+      </Text>
     </View>
+
+    {showArrow && <Image source={icons.rightArrow} className="size-5" />}
+  </TouchableOpacity>
+);
+
+const Profile = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32 px-7"
+      >
+        <View className="flex flex-row items-center justify-between mt-5">
+          <Text className="text-xl font-bold">Profile</Text>
+          <Image source={icons.bell} className="size-7" />
+        </View>
+
+        <View className="flex-row justify-center flex mt-5">
+          <View className="flex flex-col items-center relative mt-5 ">
+            <Image
+              source={images.avatar}
+              className="size-34 relative rounded-full"
+            />
+
+            <TouchableOpacity className="absolute top-16 -right-1">
+              <Image source={icons.edit} className="size-7" />
+            </TouchableOpacity>
+
+            <Text className="text-xl font-semibold mt-2">John Doe</Text>
+          </View>
+        </View>
+
+        <View className="flex flex-col mt-10">
+          <SettingsItem icon={icons.calendar} title="My Bookings" />
+          <SettingsItem icon={icons.wallet} title="Payments" />
+        </View>
+
+        <View className="flex flex-col mt-5 border-t pt-5 border-primary-100">
+          {settings.slice(2).map((item, index) => (
+            <SettingsItem key={index} {...item} />
+          ))}
+        </View>
+
+        <View className="flex flex-col border-t mt-5 pt-5 border-primary-100">
+          <SettingsItem
+            icon={icons.logout}
+            title="Log out"
+            textStyle="text-danger"
+            showArrow={false}
+            onPress={handleLogout}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
