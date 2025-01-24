af6bdc39c6dc9350a8cfd19c487f5cfe31146bc8
dilarauluturhan <dilarauluturhan@outlook.com>
2025-01-25 00:34:16 +0300

    create-readme

--- components/Cards.tsx
+++ components/Cards.tsx
@@ -1,6 +1,6 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import React from "react";
-
+import { Link } from "expo-router";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
 
@@ -31,77 +31,85 @@ const getImageByKey = (key: string) => {
 
 export const FeaturedCard = ({ data, onPress }: CardProps) => {
   return (
-    <TouchableOpacity
-      onPress={onPress}
-      className="flex flex-col items-start w-60 h-80 relative"
-    >
-      <Image
-        source={getImageByKey(data.image)}
-        className="size-full rounded-2xl"
-      />
+    <Link href={`/properties/${data.id}`} asChild>
+      <TouchableOpacity
+        onPress={onPress}
+        className="flex flex-col items-start w-60 h-80 relative"
+      >
+        <Image
+          source={getImageByKey(data.image)}
+          className="size-full rounded-2xl"
+        />
 
-      <Image
-        source={images.cardGradient}
-        className="size-full rounded-2xl absolute bottom-0"
-      />
+        <Image
+          source={images.cardGradient}
+          className="size-full rounded-2xl absolute bottom-0"
+        />
 
-      <View className="flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5">
-        <Image source={icons.star} className="size-3.5" />
-        <Text className="text-xs font-bold text-primary-100 ml-1">
-          {data.rating}
-        </Text>
-      </View>
-
-      <View className="flex flex-col items-start absolute bottom-5 inset-x-5">
-        <Text className="text-xl text-white font-extrabold" numberOfLines={1}>
-          {data.title}
-        </Text>
-        <Text className="text-base text-white" numberOfLines={1}>
-          {data.location}
-        </Text>
-
-        <View className="flex flex-row items-center justify-between w-full">
-          <Text className="text-xl font-extrabold text-white">{data.price}</Text>
-          <Image source={icons.heart} className="size-5" />
+        <View className="flex flex-row items-center bg-white/90 px-3 py-1.5 rounded-full absolute top-5 right-5">
+          <Image source={icons.star} className="size-3.5" />
+          <Text className="text-xs font-bold text-primary-100 ml-1">
+            {data.rating}
+          </Text>
         </View>
-      </View>
-    </TouchableOpacity>
+
+        <View className="flex flex-col items-start absolute bottom-5 inset-x-5">
+          <Text className="text-xl text-white font-extrabold" numberOfLines={1}>
+            {data.title}
+          </Text>
+          <Text className="text-base text-white" numberOfLines={1}>
+            {data.location}
+          </Text>
+
+          <View className="flex flex-row items-center justify-between w-full">
+            <Text className="text-xl font-extrabold text-white">
+              {data.price}
+            </Text>
+            <Image source={icons.heart} className="size-5" />
+          </View>
+        </View>
+      </TouchableOpacity>
+    </Link>
   );
 };
 
 export const Card = ({ data, onPress }: CardProps) => {
   return (
-    <TouchableOpacity
-      className="flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative"
-      onPress={onPress}
-    >
-      <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
-        <Image source={icons.star} className="size-2.5" />
-        <Text className="text-xs font-bold text-primary-100 ml-0.5">
-          {data.rating}
-        </Text>
-      </View>
-
-      <Image
-        source={getImageByKey(data.image)}
-        className="w-full h-40 rounded-lg"
-      />
-
-      <View className="flex flex-col mt-2">
-        <Text className="text-base font-bold text-black-300">{data.title}</Text>
-        <Text className="text-xs text-black-100">{data.location}</Text>
-
-        <View className="flex flex-row items-center justify-between mt-2">
-          <Text className="text-base font-bold text-primary-100">
-            {data.price}
+    <Link href={`/properties/${data.id}`} asChild>
+      <TouchableOpacity
+        className="flex-1 w-full mt-4 px-3 py-4 rounded-lg bg-white shadow-lg shadow-black-100/70 relative"
+        onPress={onPress}
+      >
+        <View className="flex flex-row items-center absolute px-2 top-5 right-5 bg-white/90 p-1 rounded-full z-50">
+          <Image source={icons.star} className="size-2.5" />
+          <Text className="text-xs font-bold text-primary-100 ml-0.5">
+            {data.rating}
           </Text>
-          <Image
-            source={icons.heart}
-            className="w-5 h-5 mr-2"
-            tintColor="#191D31"
-          />
         </View>
-      </View>
-    </TouchableOpacity>
+
+        <Image
+          source={getImageByKey(data.image)}
+          className="w-full h-40 rounded-lg"
+        />
+
+        <View className="flex flex-col mt-2">
+          <Text className="text-base font-bold text-black-300">
+            {data.title}
+          </Text>
+          <Text className="text-xs text-black-100">{data.location}</Text>
+
+          <View className="flex flex-row items-center justify-between mt-2">
+            <Text className="text-base font-bold text-primary-100">
+              {data.price}
+            </Text>
+            <Image
+              source={icons.heart}
+              className="w-5 h-5 mr-2"
+              tintColor="#191D31"
+            />
+          </View>
+        </View>
+      </TouchableOpacity>
+    </Link>
   );
 };
